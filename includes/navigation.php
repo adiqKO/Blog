b7f4e8df05306978903f32610e6b39bd7e33f52b
adiqKO <adeo.korkus@gmail.com>
2018-08-08 23:16:44 +0200

    first commit

---
+++ includes/navigation.php
@@ -0,0 +1,9 @@
+<div class="logo_div">
+    <a href="index.php"><h1>BlogTest</h1></a>
+</div>
+<ul>
+    <li><a class="active" href="index.php">Home</a></li>
+    <li><a href=" ">News</a></li>
+    <li><a href=" ">Contact</a></li>
+    <li><a href=" ">About</a></li>
+</ul>
--- index.php
+++ index.php
@@ -1,7 +1,23 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Daily
- * Date: 08.08.2018
- * Time: 22:27
- */
+<!DOCTYPE html>
+<html>
+<head>
+    <?php require_once 'includes/header.php'?>
+    <title>Strona główna</title>
+</head>
+    <body>
+        <div class="container">
+            <div class="navigation">
+                <?php require_once 'includes/navigation.php'?>
+            </div>
+
+            <div class="content">
+
+            </div>
+
+            <div class="footer">
+                <p>BlogTest &copy; <?php echo date('Y'); ?></p>
+            </div>
+
+        </div>
+    </body>
+</html>
